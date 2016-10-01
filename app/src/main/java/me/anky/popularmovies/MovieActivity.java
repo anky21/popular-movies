01b5571b6870d6317803cffc0d5c7dc01b6d63c2
Anky An <anky_au@hotmail.com>
2016-10-01 11:07:43 +1000

    Initial commit    update ic_launcher image

---
+++ app/src/main/java/me/anky/popularmovies/MovieActivity.java
@@ -0,0 +1,13 @@
+package me.anky.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movie_activity);
+    }
+}
