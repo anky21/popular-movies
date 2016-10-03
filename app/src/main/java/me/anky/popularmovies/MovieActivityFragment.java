1b1e3939c2a7d3f2463a5176bdecc6e7f9d005ea
Anky An <anky_au@hotmail.com>
2016-10-03 11:08:55 +1000

    Use Parcelable to save query data in the MainActivityFragment

--- app/src/main/java/me/anky/popularmovies/MovieActivityFragment.java
+++ app/src/main/java/me/anky/popularmovies/MovieActivityFragment.java
@@ -2,6 +2,7 @@ package me.anky.popularmovies;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -17,6 +19,8 @@ import java.util.Arrays;
 public class MovieActivityFragment extends Fragment {
 
     private PopularMovieAdapter popularMovieAdapter;
+
+    private ArrayList<PopularMovie> movieList;
 
     PopularMovie[] popularMovies = {
             new PopularMovie(R.drawable.a),
@@ -42,20 +46,37 @@ public class MovieActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(savedInstanceState == null || !savedInstanceState.containsKey("movies")) {
+            movieList = new ArrayList<PopularMovie>(Arrays.asList(popularMovies));
+        } else {
+            movieList = savedInstanceState.getParcelableArrayList("movies");
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putParcelableArrayList("movies", movieList);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.movie_activity_fragment, container, false);
-        popularMovieAdapter = new PopularMovieAdapter(getActivity(),
-                Arrays.asList(popularMovies));
+        popularMovieAdapter = new PopularMovieAdapter(getActivity(), movieList);
 
         // Get a reference to the GridView, and attach this adapter to it.
         GridView gridView = (GridView) rootView.findViewById(R.id.movie_activity_grid_view);
         gridView.setAdapter(popularMovieAdapter);
+
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(getActivity(), DetailedActivity.class);
+                PopularMovie popularMovie = popularMovieAdapter.getItem(i);
                 startActivity(intent);
             }
         });
--- app/src/main/java/me/anky/popularmovies/PopularMovie.java
+++ app/src/main/java/me/anky/popularmovies/PopularMovie.java
@@ -1,10 +1,13 @@
 package me.anky.popularmovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by anky_ on 2/10/2016.
  */
 
-public class PopularMovie {
+public class PopularMovie implements Parcelable {
     /**
      * Image resource ID for the poster
      */
@@ -19,7 +22,35 @@ public class PopularMovie {
         mPosterResourceId = posterResourceId;
     }
 
+    private PopularMovie(Parcel in){
+        mPosterResourceId=in.readInt();
+    }
+
     public int getPosterResourceId() {
         return mPosterResourceId;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(mPosterResourceId);
+    }
+
+    public final Parcelable.Creator<PopularMovie> CREATOR =
+            new Parcelable.Creator<PopularMovie>(){
+
+                @Override
+                public PopularMovie createFromParcel(Parcel parcel) {
+                    return new PopularMovie(parcel);
+                }
+
+                @Override
+                public PopularMovie[] newArray(int i) {
+                    return new PopularMovie[i];
+                }
+            };
 }
