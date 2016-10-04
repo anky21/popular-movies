a180e7c988863eff2e3d58002043cb56122bbbd4
Anky An <anky_au@hotmail.com>
2016-10-04 11:46:11 +1000

    Add more movie info into the hardcoded data

--- app/src/main/java/me/anky/popularmovies/PopularMovie.java
+++ app/src/main/java/me/anky/popularmovies/PopularMovie.java
@@ -9,25 +9,58 @@ import android.os.Parcelable;
 
 public class PopularMovie implements Parcelable {
     /**
-     * Image resource ID for the poster
+     * Movie data
      */
     private String mPosterPath;
+    private String mOriginalTitle;
+    private String mOverview;
+    private double mVoteAverage;
+    private String mRleaseDate;
 
     /**
      * Construct a new {@link PopularMovie} object
      *
      * @param posterPath is the path of the movie poster
+     * @param originalTitle is the title
+     * @param overview is the plot
+     * @param voteAverage is the rating
+     * @param releaseDate is the release date
      */
-    public PopularMovie(String posterPath) {
+    public PopularMovie(String posterPath, String originalTitle, String overview,
+                        double voteAverage, String releaseDate) {
         mPosterPath = posterPath;
+        mOriginalTitle = originalTitle;
+        mOverview = overview;
+        mVoteAverage = voteAverage;
+        mRleaseDate = releaseDate;
     }
 
     private PopularMovie(Parcel in){
-        mPosterPath=in.readString();
+        mPosterPath = in.readString();
+        mOriginalTitle = in.readString();
+        mOverview = in.readString();
+        mVoteAverage = in.readDouble();
+        mRleaseDate = in.readString();
     }
 
     public String getPosterPath() {
         return mPosterPath;
+    }
+
+    public String getOriginalTitle() {
+        return mOriginalTitle;
+    }
+
+    public String getOverview() {
+        return mOverview;
+    }
+
+    public double getVoteAverage() {
+        return mVoteAverage;
+    }
+
+    public String getRleaseDate() {
+        return mRleaseDate;
     }
 
     @Override
@@ -38,6 +71,10 @@ public class PopularMovie implements Parcelable {
     @Override
     public void writeToParcel(Parcel parcel, int i) {
         parcel.writeString(mPosterPath);
+        parcel.writeString(mOriginalTitle);
+        parcel.writeString(mOverview);
+        parcel.writeDouble(mVoteAverage);
+        parcel.writeString(mRleaseDate);
     }
 
     public static final Parcelable.Creator<PopularMovie> CREATOR =
